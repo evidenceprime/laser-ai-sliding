01ab123ded4909ae4644d6d3f2f84568cb9d5093
weterynarzfred <weterynarzfred@gmail.com>
2022-09-20 12:16:06 +0200

    grouping blocks

--- src/js/main.js
+++ src/js/main.js
@@ -8,7 +8,6 @@ const MIN_BLOCK_WIDTH = 2;
 const MAX_BLOCK_WIDTH = 8;
 const DEFAULT_UNIT_SIZE = 30;
 const MAX_SLIDE_DELAY = 200;
-const MAX_HIDE_DELAY = 3000;
 const MAX_BLOCK_INTERVAL = 500;
 const ADDITIONAL_DELAY = 1000;
 const X_FILL = 0.9;
@@ -45,7 +44,14 @@ function resize() {
 
 function appendBlock(rows) {
   const rowIndex = getRandomInt(0, RESOLUTION.y - 1);
-  if (rows[rowIndex] !== undefined) return;
+
+  if (rows[rowIndex] === undefined) {
+    rows[rowIndex] = {
+      width: getRandomInt(0, RESOLUTION.x / 10),
+      blocks: [],
+    };
+  }
+  if (rows[rowIndex].width >= RESOLUTION.x / 3) return;
 
   const blockWidth = getRandomInt(MIN_BLOCK_WIDTH, MAX_BLOCK_WIDTH);
   const block = {
@@ -53,62 +59,100 @@ function appendBlock(rows) {
     element: $('<div class="block">').appendTo(containerElement),
     width: blockWidth,
     x: Math.floor((RESOLUTION.x - blockWidth) * Math.random() + RESOLUTION.x / 2),
+    finalX: rows[rowIndex].width,
     y: rowIndex,
     transition: '.5s',
   };
 
+  const initialOffset = block.width * Math.random();
   block.element.css({
-    width: UNIT.x * block.width + '%',
+    width: 0,
     height: UNIT.y + '%',
     top: UNIT.y * (block.y + OFFSET.y) + '%',
-    left: UNIT.x * (block.x + OFFSET.x) + '%',
+    left: UNIT.x * (block.x + OFFSET.x + initialOffset) + '%',
     backgroundColor: getRandomColor(),
-    opacity: 0,
   });
 
-  rows[rowIndex] = block;
+  rows[rowIndex].blocks.push(block);
+  rows[rowIndex].width += block.width;
   return block;
 }
 
 function revealBlock(block) {
   block.element.css({
-    opacity: 1,
+    width: UNIT.x * block.width + '%',
+    left: UNIT.x * (block.x + OFFSET.x) + '%',
   });
 }
 
 function slideBlock(block) {
-  block.finalX = Math.floor((RESOLUTION.x / 3 - block.width) * Math.random());
   block.element.css({
     left: UNIT.x * (block.finalX + OFFSET.x) + '%',
   });
 }
 
 function hideBlock(block) {
-  block.element.css({
-    opacity: 0,
-  });
+  if (Math.random() > 0.5) {
+    block.element.css({
+      opacity: 0,
+    });
+  } else {
+    block.element.css({
+      left: -(10 + block.width * UNIT.x) + '%',
+    });
+  }
 }
 
-function removeBlock(block, rows) {
+function removeBlock(block) {
   block.element.remove();
-  rows[block.index] = undefined;
+}
+
+function clearRows(filledRows) {
+  if (filledRows.length >= RESOLUTION.y / 5) {
+    filledRows.forEach(row => {
+      row.blocks.forEach(block => {
+        setTimeout(() => hideBlock(block), MAX_SLIDE_DELAY + ADDITIONAL_DELAY + 100);
+        setTimeout(() => removeBlock(block), MAX_SLIDE_DELAY + ADDITIONAL_DELAY + 1000);
+      });
+      row.width = getRandomInt(0, RESOLUTION.x / 10);
+      row.blocks = [];
+    });
+  }
+}
+
+function checkRows() {
+  let filledRows = [];
+
+  for (let i = 0; i < rows.length; i++) {
+    const row = rows[i];
+    if (
+      row === undefined ||
+      (row.width < 6 && row.blocks.length < 2)
+    ) {
+      clearRows(filledRows);
+      filledRows = [];
+    } else {
+      filledRows.push(row);
+    }
+  }
+  clearRows(filledRows);
 }
 
 function play() {
   const block = appendBlock(rows);
   if (block !== undefined) {
     const slideDelay = MAX_SLIDE_DELAY * Math.random() + 100 + ADDITIONAL_DELAY;
-    const hideDelay = MAX_HIDE_DELAY * Math.random() + slideDelay + ADDITIONAL_DELAY;
 
     setTimeout(() => revealBlock(block), 100);
+    setTimeout(() => slideBlock(block), 500);
     setTimeout(() => slideBlock(block), slideDelay);
-    setTimeout(() => hideBlock(block), hideDelay);
-    setTimeout(() => removeBlock(block, rows), hideDelay + 1000);
   }
 
+  checkRows();
   setTimeout(play, MAX_BLOCK_INTERVAL * Math.random());
 }
 
 window.addEventListener('resize', resize);
 resize();
 play();
+
